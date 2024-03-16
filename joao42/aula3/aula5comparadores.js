c0fd2efd9ac8590f505fa1abdf0174fe67aa839c
joaomrk <162516828+joaomrk@users.noreply.github.com>
2024-03-15 21:54:29 -0300

    Add files via upload

---
+++ joao42/aula3/aula5comparadores.js
@@ -0,0 +1,11 @@
+number1 = 4
+number2 = 5
+
+resultado = number1 === number2 
+console.log("a.",resultado)
+resultado = number1 !== number2
+console.log("b.",resultado)
+resultado = number1 > number2
+console.log("c.",resultado)
+resultado = number1 < number2
+console.log("d.", resultado)
---
+++ joao42/aula3/aula5exloop.js
@@ -0,0 +1,15 @@
+let myAge = 23
+let friendAge = 16
+console.log("sua idade é maior que a do seu amigo?",myAge > friendAge)
+console.log(myAge % friendAge)
+console.log(friendAge % 2)
+//todos os numeros pares que forem divisiveis por 2 tem a diferença de numero sempre igual a ZERO
+console.log(myAge %2)
+//se for um numero impar a resposta será 1
+inMonths = myAge * 12
+inDays = inMonths * 30
+inHours = inDays * 24
+console.log(inMonths, inDays, inHours) 
+//foi impresso nessa ordem 276 8280 198720
+let num1
+let num2 
